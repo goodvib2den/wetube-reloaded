c612a82dc04749a167b6383b998047397bf45900
eden <gmean7@gmail.com>
2021-11-20 19:41:59 +0900

    Set Frist preview

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,34 @@
 export const trending = (req, res) => {
-  const videos = [];
+  const videos = [
+    {
+      title: "1 Video",
+      rating: 5,
+      comments: 2,
+      crfeatedAt: "2 minutes ago",
+      views: 50,
+      id: 1,
+    },
+    {
+      title: "2 Video",
+      rating: 5,
+      comments: 2,
+      crfeatedAt: "2 minutes ago",
+      views: 50,
+      id: 1,
+    },
+    {
+      title: "3 Video",
+      rating: 5,
+      comments: 2,
+      crfeatedAt: "2 minutes ago",
+      views: 50,
+      id: 1,
+    },
+  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const edit = (req, res) => res.send("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
