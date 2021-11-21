ba38bbf47a70809634d3feadd15cd8cfe910344e
eden <gmean7@gmail.com>
2021-11-21 15:35:58 +0900

    Arr db practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,36 +1,39 @@
+let videos = [
+  {
+    title: "1 Video",
+    rating: 5,
+    comments: 2,
+    crfeatedAt: "2 minutes ago",
+    views: 50,
+    id: 1,
+  },
+  {
+    title: "2 Video",
+    rating: 5,
+    comments: 2,
+    crfeatedAt: "2 minutes ago",
+    views: 50,
+    id: 2,
+  },
+  {
+    title: "3 Video",
+    rating: 5,
+    comments: 2,
+    crfeatedAt: "2 minutes ago",
+    views: 50,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "1 Video",
-      rating: 5,
-      comments: 2,
-      crfeatedAt: "2 minutes ago",
-      views: 50,
-      id: 1,
-    },
-    {
-      title: "2 Video",
-      rating: 5,
-      comments: 2,
-      crfeatedAt: "2 minutes ago",
-      views: 50,
-      id: 1,
-    },
-    {
-      title: "3 Video",
-      rating: 5,
-      comments: 2,
-      crfeatedAt: "2 minutes ago",
-      views: 50,
-      id: 1,
-    },
-  ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const see = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+};
 export const edit = (req, res) => res.send("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-  return res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
