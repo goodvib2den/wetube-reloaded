68b3a937aea18df95729e80973089f4b8f3e926b
eden <gmean7@gmail.com>
2021-12-07 11:14:47 +0900

    ADD Change password function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,8 @@ import User from "../models/User";
 import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
+const HTTP_400 = 400;
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 
 export const postJoin = async (req, res) => {
@@ -11,7 +13,7 @@ export const postJoin = async (req, res) => {
   // password confrim
   if (password !== password2) {
     // 오류가 있을시 여기서 return을 걸어줘야 함수가 중단될 수 있기 떄문에 return을 달아준다
-    return res.status(400).render("join", {
+    return res.status(HTTP_400).render("join", {
       pageTitle,
       errorMessage: "Passeord confirmation does not match",
     });
@@ -20,7 +22,7 @@ export const postJoin = async (req, res) => {
   // 유저이름과 이메일이 중복되는지 확인
   const exists = await User.exists({ $or: [{ username }, { email }] });
   if (exists) {
-    return res.status(400).render("join", {
+    return res.status(HTTP_400).render("join", {
       pageTitle,
       errorMessage: "This username or email is already used",
     });
@@ -37,7 +39,7 @@ export const postJoin = async (req, res) => {
     });
     return res.redirect("/login");
   } catch (error) {
-    return res.status(400).render("join", {
+    return res.status(HTTP_400).render("join", {
       pageTitle: "Join",
       errorMessage: error._message,
     });
@@ -53,7 +55,7 @@ export const postLogin = async (req, res) => {
   const pageTitle = "Login";
   const user = await User.findOne({ username, socialOnly: false });
   if (!user) {
-    return res.status(400).render("login", {
+    return res.status(HTTP_400).render("login", {
       pageTitle,
       errorMessage: "An account with this username does not exists.",
     });
@@ -62,7 +64,7 @@ export const postLogin = async (req, res) => {
   // 로그인하는 패스워드 확인
   const ok = await bcrypt.compare(password, user.password);
   if (!ok) {
-    return res.status(400).render("login", {
+    return res.status(HTTP_400).render("login", {
       pageTitle,
       errorMessage: "Wrong Password",
     });
@@ -167,7 +169,7 @@ export const postEdit = async (req, res) => {
   const findUsername = await User.findOne({ username });
   const findEmail = await User.findOne({ email });
   if (findUsername._id != _id || findEmail._id != _id) {
-    return res.status(400).render("edit-profile", {
+    return res.status(HTTP_400).render("edit-profile", {
       pageTitle: "Edit Profile",
       errorMessage: "이미 있는 이름이거나 이메일입니다.",
     });
@@ -184,6 +186,39 @@ export const postEdit = async (req, res) => {
   );
   req.session.user = updatedUser;
   return res.redirect("/users/edit");
+};
+
+export const getChangePassword = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    return res.redirect("/");
+  }
+  return res.render("users/change-password", { pageTitle: "Change Password" });
+};
+
+export const postChangePassword = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { oldPassword, newPassword, newPasswordComf },
+  } = req;
+  const user = await User.findById(_id);
+  const ok = await bcrypt.compare(oldPassword, user.password);
+  if (!ok) {
+    return res.status(HTTP_400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "기존 비밀번호가 일치하지 않습니다.",
+    });
+  }
+  if (newPassword !== newPasswordComf) {
+    return res.status(HTTP_400).render("users/change-password", {
+      pageTitle: "Change Password",
+      errorMessage: "새로운 비밀번호가 서로 일치하지 않습니다.",
+    });
+  }
+  user.password = newPassword;
+  await user.save();
+  return res.redirect("/");
 };
 
 export const see = (req, res) => res.send("See");
