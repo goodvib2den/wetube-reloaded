2e7a19bea88e9aaae2af32367c51e22fffffbba9
eden <gmean7@gmail.com>
2021-12-08 10:54:17 +0900

    ADD upload avatar image

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -162,9 +162,10 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
   const {
     session: {
-      user: { _id },
+      user: { _id, avatarUrl },
     },
     body: { name, email, username, location },
+    file,
   } = req;
   const findUsername = await User.findOne({ username });
   const findEmail = await User.findOne({ email });
@@ -177,6 +178,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
+      avatarUrl: file ? file.path : avatarUrl,
       name,
       email,
       username,
