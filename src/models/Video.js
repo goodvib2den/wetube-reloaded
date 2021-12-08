f4279c1faf49b2ce6f5872ce65398cba30d15f5c
eden <gmean7@gmail.com>
2021-12-08 11:20:14 +0900

    ADD Upload video

--- src/models/Video.js
+++ src/models/Video.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 // 하나의 구조를 가질 수 있게끔 db를 구조하는 것이 아래의 한 블록
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxlength: 80 },
+  fileUrl: { type: String, required: true },
   description: { type: String, required: true, trim: true, minlength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
