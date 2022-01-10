8c985bd198b1c76dac55b89817848db345991693
eden <gmean7@gmail.com>
2022-01-10 17:23:49 +0900

    Start Btn event fix

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,7 +1,19 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
-const handleStart = async () => {
+const handleStop = () => {
+  startBtn.innerText = "Start recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+};
+
+const init = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
@@ -9,5 +21,7 @@ const handleStart = async () => {
   preview.srcObject = stream;
   preview.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
