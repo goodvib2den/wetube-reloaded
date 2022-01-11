69f9acfcc663a9a266dc5100456c52ba80c0cd98
eden <gmean7@gmail.com>
2022-01-11 14:11:23 +0900

    ADD video MediaRecorder code

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,8 @@
 const startBtn = document.getElementById("startBtn");
 const preview = document.getElementById("preview");
 
+let stream;
+
 const handleStop = () => {
   startBtn.innerText = "Start recording";
   startBtn.removeEventListener("click", handleStop);
@@ -11,10 +13,16 @@ const handleStart = () => {
   startBtn.innerText = "Stop recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => console.log(e);
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
 };
 
 const init = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
